0b70f0ca354d16239cdd331dd31c5ec76f181b79
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-17 18:47:03 +0000

    refactoring and implement index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
-const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const arg = process.argv[2];
+const URL = 'https://api.thecatapri.com/v1/breeds/search?q=';
 
-request(URL + arg, (error, response, body) => {
-  if (error) {
-    console.log('error: ', error);
-    console.log('statusCode: ', response && response.statusCode);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Breed not found.');
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(URL + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null, 'Breed not found.');
+      } else {
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {fetchBreedDescription};
